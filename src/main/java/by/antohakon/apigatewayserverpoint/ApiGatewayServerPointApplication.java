4339f348ee5177866407f53c4d70b7b8fb9ac57a
AntonP17 <pokrovskii.anton@mail.ru>
2025-10-22 20:06:41 +0300

    create api-gateway

---
+++ src/main/java/by/antohakon/apigatewayserverpoint/ApiGatewayServerPointApplication.java
@@ -0,0 +1,15 @@
+package by.antohakon.apigatewayserverpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayServerPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServerPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/antohakon/apigatewayserverpoint/ApiGatewayServerPointApplicationTests.java
@@ -0,0 +1,13 @@
+package by.antohakon.apigatewayserverpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
